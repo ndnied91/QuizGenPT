381f72d3af5b294b3d85f67cf32d257c15f92f6f
Danny Niedzwiedzki <danielniedzwiedzki.1@gmail.com>
2024-07-09 18:53:01 -0400

    update to backend

--- frontend/src/utils/util.js
+++ frontend/src/utils/util.js
@@ -1,6 +1,22 @@
+// import axios from 'axios';
+// const customFetch = axios.create({
+//   baseURL: 'http://localhost:8000/api',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// export default customFetch;
+
 import axios from 'axios';
+
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://your-backend.onrender.com/api'
+    : 'http://localhost:8000/api';
+
 const customFetch = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
--- main.py
+++ main.py
@@ -1,30 +1,36 @@
-from fastapi import FastAPI , Query , Body
+from fastapi import FastAPI, HTTPException, status, Query, Body
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
-from pydantic import BaseModel, Field 
+from pydantic import BaseModel, Field
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import List, Optional, Dict, Any
+from bson import ObjectId
+from datetime import datetime
+import json
+import os
+import pytz
+from dotenv import load_dotenv
 
 from utils import generate_question
 
-
-from fastapi import FastAPI, HTTPException , status
-from motor.motor_asyncio import AsyncIOMotorClient
-from pydantic import BaseModel
-from typing import List, Optional
-from pymongo.server_api import ServerApi
-from bson import ObjectId
-import json
-from datetime import datetime
-from typing import Dict, Any
-from pymongo import ReturnDocument
-
-
-from dotenv import load_dotenv  # Import the load_dotenv function
-import os
-import pytz
-
-load_dotenv() 
+load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MONGO_DETAILS = os.getenv("MONGO_URI")
+client = AsyncIOMotorClient(MONGO_DETAILS, tlsAllowInvalidCertificates=True)
+
+database = client['QuizDatabase']
+collection = database["UserQuizzes"]
+
 
 
 class Question(BaseModel):
@@ -38,22 +44,6 @@ class URL_Question(BaseModel):
     questionCount: int
     questionType: str
     difficulty: str
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-MONGO_DETAILS = os.getenv("MONGO_URI")
-client = AsyncIOMotorClient(MONGO_DETAILS, tlsAllowInvalidCertificates=True)
-
-database = client['QuizDatabase']
-collection = database["UserQuizzes"]
 
 class Item(BaseModel):
     id: Optional[str]
@@ -91,16 +81,28 @@ except Exception as e:
     print("error")
 
 
+async def generate_and_insert_quiz(user: str, quiz_string: str) -> Any:
+    try:
+        quiz_object = json.loads(quiz_string)
+    except json.JSONDecodeError:
+        return {"error": "Invalid quiz data received"}
+
+    if user != 'null':
+        quiz_item = QuizItem(user=user, quiz=quiz_object)
+        quiz_item_dict = quiz_item.dict(by_alias=True)
+        db_response = await collection.insert_one(quiz_item_dict)
+        inserted_document = await collection.find_one({"_id": db_response.inserted_id})
+        return inserted_document
+    else:
+        return quiz_object
+
+
  
-@app.get("/api")
-async def read_root():
-    return {"message": "Welcome to the FastAPI + MongoDB app"}
-
-
 @app.get("/api/quiz", response_model=QuizItem)
 async def get_active_quiz_item(user: str = Query(...)):
     # Retrieve a single document where is_active is True and user matches the query parameter
     quiz_item = await collection.find_one({"is_active": True, "user": user})
+    print(quiz_item)
 
     if quiz_item is None:
         raise HTTPException(status_code=204, detail="No active quiz item found for the specified user")
@@ -108,100 +110,32 @@ async def get_active_quiz_item(user: str = Query(...)):
 
 
 @app.post("/api/quiz/article")
-async def generate_quiz(user: str, url_question: URL_Question):
+async def generate_quiz_from_article(user: str, url_question: URL_Question):
     quiz_string = generate_question(
         f"This is a URL from an article: {url_question.article}, number of questions: {url_question.questionCount}, questionType of quiz: {url_question.questionType}, difficulty: {url_question.difficulty}, please JUST provide the data in the object form."
     )
-    print(quiz_string)
-    try: # Convert the quiz string to an object
-        quiz_object = json.loads(quiz_string)
-        print(quiz_object)
-    except json.JSONDecodeError:
-        return {"error": "Invalid quiz data received"}
-    # Create a QuizItem instance
-    
-    if user != 'null':
-        quiz_item = QuizItem(user=user, quiz=quiz_object)
-        # Convert the QuizItem instance to a dictionary
-        quiz_item_dict = quiz_item.dict(by_alias=True)  # Use by_alias=True to map `id` to `_id`
-        # Insert the document into the MongoDB collection
-        db_response = await collection.insert_one(quiz_item_dict)
-
-        # Get the inserted_id from the db_response
-        inserted_id = db_response.inserted_id
-    
-        # Retrieve the inserted document from the database
-        inserted_document = await collection.find_one({"_id": inserted_id})
-        return inserted_document
-    else:
-        return quiz_object
-    
-
-
-
-@app.post("/api/quiz/{_id}")
-async def update_item(_id: str, body: UpdateRequest = Body(...)):
-    user = body.user
-    to_update = body.to_update
-    
-    print("ID:", _id)
-    print("User:", user)
-    print("To Update:", to_update)
-
-    quiz_item = await collection.find_one({"_id": _id, "user": user})
-    
-    if not quiz_item:
-        raise HTTPException(status_code=404, detail="Item not found")
-    
-
-    # update_data = {"$set": to_update }
-    await collection.find_one_and_update({"_id": _id, "user": user}, {"$set": to_update })
-
-    return Response(status_code=status.HTTP_200_OK)
+    return await generate_and_insert_quiz(user, quiz_string)
 
 
 @app.post("/api/quiz")
 async def generate_quiz(user: str, question: Question):
-    # Generate the quiz based on the question
     quiz_string = generate_question(
         f"Question category: {question.category}, number of questions: {question.questionCount}, questionType of quiz: {question.questionType}, difficulty: {question.difficulty}"
     )
+    return await generate_and_insert_quiz(user, quiz_string)
+
     
-    try: # Convert the quiz string to an object
-        quiz_object = json.loads(quiz_string)
-    except json.JSONDecodeError:
-        return {"error": "Invalid quiz data received"}
-    # Create a QuizItem instance
-    
+@app.post("/api/quiz/{_id}")
+async def update_item(_id: str, body: UpdateRequest = Body(...)):
+    user = body.user
+    to_update = body.to_update
 
-    if user != 'null':
-        quiz_item = QuizItem(user=user, quiz=quiz_object)
-        # Convert the QuizItem instance to a dictionary
-        quiz_item_dict = quiz_item.dict(by_alias=True)  # Use by_alias=True to map `id` to `_id`
-        # Insert the document into the MongoDB collection
-        db_response = await collection.insert_one(quiz_item_dict)
+    quiz_item = await collection.find_one({"_id": _id, "user": user})
+    if not quiz_item:
+        raise HTTPException(status_code=404, detail="Item not found")
 
-        # Get the inserted_id from the db_response
-        inserted_id = db_response.inserted_id
-    
-        # Retrieve the inserted document from the database
-        inserted_document = await collection.find_one({"_id": inserted_id})
-        return inserted_document
-    else:
-        return quiz_object
-
-
-
-# @app.put("/api/archives/update/{user}")
-# async def reactivate_quiz(user: str = Query(...)):
-#     # Retrieve a single document where is_active is True and user matches the query parameter
-#     quiz_item = await collection.find_one({"is_active": False, "user": user})
-
-#     if quiz_item is None:
-#         raise HTTPException(status_code=204, detail="No active quiz item found for the specified user")
-#     return quiz_item
-
-
+    await collection.find_one_and_update({"_id": _id, "user": user}, {"$set": to_update})
+    return Response(status_code=status.HTTP_200_OK)
 
 @app.get("/api/archives/{user}")
 async def get_user_archives(user: str):
@@ -211,16 +145,13 @@ async def get_user_archives(user: str):
         raise HTTPException(status_code=204, detail="No archived quiz items found for the specified user")
     return quiz_items
 
-
-
-
+@app.delete("/api/quiz/{_id}")
+async def remove_item(_id: str, user: str = Query(...)):
+    quiz_item = await collection.delete_one({"_id": _id, "user": user})
+    if quiz_item.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return Response(status_code=status.HTTP_200_OK)
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
-
-
-
