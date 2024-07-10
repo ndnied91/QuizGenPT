dda27e479f2af86c63bca2387d2410882a0e0bf4
Danny Niedzwiedzki <danielniedzwiedzki.1@gmail.com>
2024-07-09 21:39:59 -0400

    update to config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,20 +4,32 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist', // This is the default, but ensure it's set if you've customized it
+//   },
+//   define: {
+//     'process.env': process.env,
+//   },
+//   proxy: {
+//     '/api': {
+//       target: 'http://localhost:8000/api',
+//       changeOrigin: true,
+//       rewrite: (path) => path.replace(/^\/api/, ''),
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
   build: {
-    outDir: 'dist', // This is the default, but ensure it's set if you've customized it
-  },
-  proxy: {
-    '/api': {
-      target: 'http://localhost:8000/api',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
+    outDir: 'dist', // Ensure this is set to 'dist'
+    rollupOptions: {
+      output: {
+        sourcemap: true, // Optional: useful for debugging
+      },
     },
   },
 });
